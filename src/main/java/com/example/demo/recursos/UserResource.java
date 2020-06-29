a31eb8d503ed77ec0b86ec6fd0ad929883f5fd50
GiovaniCenta <geeowdk@gmail.com>
2020-06-29 13:39:25 -0300

    Insercao, deleção, atualização de user

--- src/main/java/com/example/demo/recursos/UserResource.java
+++ src/main/java/com/example/demo/recursos/UserResource.java
@@ -1,17 +1,24 @@
 package com.example.demo.recursos;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.entidades.User;
 import com.example.demo.servicos.UserService;
+
 
 @RestController //Serve para dizer que é um recurso web controlado por um rest controller(resource)
 @RequestMapping(value = "/users",method=RequestMethod.GET) //É o caminho do meu recurso
@@ -32,7 +39,26 @@ public class UserResource { //disponibiliza um recurso WEB correspondente a clas
 		return ResponseEntity.ok().body(obj);
 	}
 	
-
+	@PostMapping //pré processamento pro compilador entender um método de inserção
+	public ResponseEntity<User> insert(@RequestBody User obj){  //precisa da anotação para fazer a serialização e desserialização do objeto
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(obj); //created espera a localizacao do objeto, por tanto tem que pegar o endereço dela pelo URI
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();  //retorna uma resposta no content, uma resposta vazia, o codigo de eero que nao existe é 204
+		//Erro 500 é quando tem pedidos associados a esse usuario, o que faz o banco de dados nao permitir a deleçao
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<User> update (@PathVariable Integer id,@RequestBody User obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
 	
 
 }
--- src/main/java/com/example/demo/servicos/UserService.java
+++ src/main/java/com/example/demo/servicos/UserService.java
@@ -27,4 +27,26 @@ public class UserService {
 		return obj.get(); //retorna o obj optional
 	}
 
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		repository.deleteById(id);
+	}
+	
+	public User update(Integer id, User obj) {
+		User entity=repository.getOne(id);
+		updateData(entity,obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+		
+		
+	}
 }
